05aa767c93230e4b2b9d3566fef79620c6190e49
lleontor <luisleon705@gmail.com>
2024-08-10 22:48:45 -0500

    Carga De proyecto

--- BDHelper/DatabaseHandlerFactory.cs
+++ BDHelper/DatabaseHandlerFactory.cs
@@ -1,37 +1,82 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Configuration;
+using static ATS.PROYECT.UTIL.Constant.Constants;
 
 namespace ATS.PROYECT.UTIL.BDHelper
 {
     public class DatabaseHandlerFactory
     {
         private readonly ConnectionStringSettings connectionStringSettings;
+        private readonly string providerName;
 
+        // Constructor para .NET Framework (Web.config / App.config)
+        /// <summary>
+        /// Constructor para .NET Framework (Web.config / App.config)
+        /// </summary>
+        /// <param name="connectionName"></param>
+        /// <exception cref="ArgumentException"></exception>
         public DatabaseHandlerFactory(string connectionName)
         {
             connectionStringSettings = ConfigurationManager.ConnectionStrings[connectionName];
             if (connectionStringSettings == null)
             {
-                throw new ArgumentException($"No se encontró la cadena de conexión con el nombre '{connectionName}' en el archivo Web.config.");
+                throw new ArgumentException($"No se encontró la cadena de conexión con el nombre '{connectionName}' en el archivo Web.config o App.config.");
+            }
+
+            providerName = connectionStringSettings.ProviderName;
+        }
+
+        // Constructor para .NET Core (appsettings.json)
+        /// <summary>
+        /// Constructor para .NET Core (appsettings.json)
+        /// </summary>
+        /// <param name="connectionName"></param>
+        /// <param name="configuration"></param>
+        /// <param name="useNetCoreConfig"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public DatabaseHandlerFactory(string connectionName, IConfiguration configuration, bool useNetCoreConfig)
+        {
+            if (useNetCoreConfig)
+            {
+                var connectionString = configuration.GetConnectionString(connectionName);
+                providerName = configuration.GetSection($"ConnectionStrings:{connectionName}:ProviderName").Value;
+
+                if (string.IsNullOrEmpty(connectionString) || string.IsNullOrEmpty(providerName))
+                {
+                    throw new ArgumentException($"No se encontró la cadena de conexión o el proveedor con el nombre '{connectionName}' en el archivo appsettings.json.");
+                }
+
+                connectionStringSettings = new ConnectionStringSettings(connectionName, connectionString, providerName);
+            }
+            else
+            {
+                connectionStringSettings = ConfigurationManager.ConnectionStrings[connectionName];
+                if (connectionStringSettings == null)
+                {
+                    throw new ArgumentException($"No se encontró la cadena de conexión con el nombre '{connectionName}' en el archivo Web.config o App.config.");
+                }
+
+                providerName = connectionStringSettings.ProviderName;
             }
         }
 
         public IDataAccess CrearDatabase()
         {
             IDataAccess database = null;
-            switch (connectionStringSettings.ProviderName.ToLower())
+            switch (providerName.ToLower())
             {
-                case "system.data.sqlclient":
+                case DataBaseType.SQLCLIENT:
                     database = new SqlDataAccess(connectionStringSettings.ConnectionString);
                     break;
-                case "npgsql":
+                case DataBaseType.POSTGRESQL:
                     database = new PostgreDataAccess(connectionStringSettings.ConnectionString);
                     break;
-                case "mysql.data.mysqlclient":
+                case DataBaseType.MYSQLCLIENT:
                     database = new MySqlDataAccess(connectionStringSettings.ConnectionString);
                     break;
                 default:
-                    throw new NotSupportedException($"El proveedor '{connectionStringSettings.ProviderName}' no está soportado.");
+                    throw new NotSupportedException($"El proveedor '{providerName}' no está soportado.");
             }
 
             return database;
@@ -39,9 +84,8 @@ namespace ATS.PROYECT.UTIL.BDHelper
 
         public string ObtenerNombreProveedor()
         {
-            return connectionStringSettings.ProviderName;
+            return providerName;
         }
     }
-
 
 }
