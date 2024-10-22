4b88db41a567200e2c4d53492ea1640a61a346ba
lleontor <14917127+lleontor705@users.noreply.github.com>
2024-10-22 13:11:16 -0500

    Update CallApiClientExternal.cs

--- CallApiClient/CallApiClientExternal.cs
+++ CallApiClient/CallApiClientExternal.cs
@@ -94,7 +94,9 @@ namespace ATS.PROYECT.UTIL.CallApiClient
         public static bool ValidateServerCertificate(Object sender, System.Security.Cryptography.X509Certificates.X509Certificate certificate,
             System.Security.Cryptography.X509Certificates.X509Chain chain, System.Net.Security.SslPolicyErrors sslPolicyErrors)
         {
-            return true;
+              if (sslPolicyErrors == System.Net.Security.SslPolicyErrors.None)
+                    return true; 
+                return false;
         }
     }
 }
